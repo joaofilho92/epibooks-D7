4cb9d6e20a18ef9ad156555bf7674d3c0fe7d989
Joao Batista Lopes Filho <batistafilho.lopes@gmail.com>
2023-04-04 17:01:01 +0200

    epibooks-D7

---
+++ src/components/CommentArea.jsx
@@ -0,0 +1,85 @@
+import { Component } from 'react'
+import CommentList from './CommentList'
+import AddComment from './AddComment'
+import Loading from './Loading'
+import Error from './Error'
+
+class CommentArea extends Component {
+  state = {
+    comments: [],
+    isLoading: false,
+    isError: false,
+  }
+
+  // componentDidMount = async () => {
+  //   try {
+  //     let response = await fetch(
+  //       'https://striveschool-api.herokuapp.com/api/comments/' +
+  //         this.props.asin,
+  //       {
+  //         headers: {
+  //           Authorization: ' your-auth-token-goes-here',
+  //         },
+  //       }
+  //     )
+  //     console.log(response)
+  //     if (response.ok) {
+  //       let comments = await response.json()
+  //       this.setState({ comments: comments, isLoading: false, isError: false })
+  //     } else {
+  //       console.log('error')
+  //       this.setState({ isLoading: false, isError: true })
+  //     }
+  //   } catch (error) {
+  //     console.log(error)
+  //     this.setState({ isLoading: false, isError: true })
+  //   }
+  // }
+
+  componentDidUpdate = async (prevProps) => {
+    if (prevProps.asin !== this.props.asin) {
+      this.setState({
+        isLoading: true,
+      })
+      try {
+        let response = await fetch(
+          'https://striveschool-api.herokuapp.com/api/comments/' +
+            this.props.asin,
+          {
+            headers: {
+              Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDE0NGIwM2Y4MWI0MjAwMTM5YjI4ZjgiLCJpYXQiOjE2ODA2MTM4MzUsImV4cCI6MTY4MTgyMzQzNX0.HLZvlpQs0lRj_jRURBqMSXtSo4snsu096dyyvKJ32rE',
+            },
+          }
+        )
+        console.log(response)
+        if (response.ok) {
+          let comments = await response.json()
+          this.setState({
+            comments: comments,
+            isLoading: false,
+            isError: false,
+          })
+        } else {
+          console.log('error')
+          this.setState({ isLoading: false, isError: true })
+        }
+      } catch (error) {
+        console.log(error)
+        this.setState({ isLoading: false, isError: true })
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div className="text-center">
+        {this.state.isLoading && <Loading />}
+        {this.state.isError && <Error />}
+        <AddComment asin={this.props.asin} />
+        <CommentList commentsToShow={this.state.comments} />
+      </div>
+    )
+  }
+}
+
+export default CommentArea
---
+++ src/components/SingleComment.jsx
@@ -0,0 +1,39 @@
+import { Button, ListGroup } from 'react-bootstrap'
+
+const SingleComment = ({ comment }) => {
+  const deleteComment = async (asin) => {
+    try {
+      let response = await fetch(
+        'https://striveschool-api.herokuapp.com/api/comments/' + asin,
+        {
+          method: 'DELETE',
+          headers: {
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDE0NGIwM2Y4MWI0MjAwMTM5YjI4ZjgiLCJpYXQiOjE2ODA2MTM4MzUsImV4cCI6MTY4MTgyMzQzNX0.HLZvlpQs0lRj_jRURBqMSXtSo4snsu096dyyvKJ32rE',
+          },
+        }
+      )
+      if (response.ok) {
+        alert('Comment was deleted successfully!')
+      } else {
+        alert('Error - comment was NOT deleted!')
+      }
+    } catch (error) {
+      alert('Error - comment was NOT deleted!')
+    }
+  }
+
+  return (
+    <ListGroup.Item>
+      {comment.comment}
+      <Button
+        variant="danger"
+        className="ml-2"
+        onClick={() => deleteComment(comment._id)}
+      >
+        Delete
+      </Button>
+    </ListGroup.Item>
+  )
+}
+
+export default SingleComment
